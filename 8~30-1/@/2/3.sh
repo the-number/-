8ae22ef5b47fe648ca93907bf688ff4753a95620
Gunter Liszewski (Pi02) <gunter.liszewski@gmail.com>
2021-08-24 02:32:47 +0100

    Pi Zero W: about paths
    Signed-off-by: Gunter Liszewski (Pi02) <gunter.liszewski@gmail.com>

---
+++ 8~30-1/@/2/3.sh
@@ -0,0 +1,3 @@
+make 2 \
+     GUILE_LOAD_PATH=/usr/local/share/guile-lib/ \
+     GUILE_LOAD_COMPILED_PATH=/usr/local/lib/guile-lib/guile/3.0/site-ccache/
---
+++ 8~30-1/@/2/4.sh
@@ -0,0 +1,1 @@
+make 3 GUILE_LOAD_COMPILED_PATH=/usr/local/lib/guile-lib/guile/3.0/site-ccache/
